a39a4208b7853d073fd97600b1483ee9be2e022c
yoodain1027 <yoodain1027@naver.com>
2025-07-18 01:52:56 +0900

    Create routes.js

---
+++ backend/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('./controller/postController');
+const commentController = require('./controller/commentController');
+const { authenticateUser } = require('./middleware/auth');
+
+// 📝 게시글 라우팅
+router.post('/posts', authenticateUser, postController.createPost);
+router.put('/posts/:id', authenticateUser, postController.updatePost);
+router.delete('/posts/:id', authenticateUser, postController.deletePost);
+
+// 💬 댓글 라우팅
+router.post('/comments', authenticateUser, commentController.createComment);
+router.put('/comments/:id', authenticateUser, commentController.updateComment);
+router.delete('/comments/:id', authenticateUser, commentController.deleteComment);
+
+module.exports = router;
