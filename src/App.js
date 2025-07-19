0aceb3b12092270bda7c95260dd92d62fd451473
yoodain1027 <yoodain1027@naver.com>
2025-07-19 17:08:54 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Mainpage from "./pages/Mainpage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import FindAccount from "./pages/Findaccount";
+import BoardPage from "./pages/BoardPage";
+import PostDetailPage from "./pages/PostDetailPage";
+import EditPostPage from "./pages/EditPostPage";
+import DeletePostPage from "./pages/DeletePostPage";
+import EditCommentPage from "./pages/EditCommentPage";
 
 function App() {
   return (
@@ -16,6 +21,11 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/find" element={<FindAccount />} />
+          <Route path="/boardpage" element={<BoardPage />} />
+          <Route path="/editpostpage" element={<EditPostPage />} />
+          <Route path="/postdetailpage" element={<PostDetailPage />} />
+          <Route path="/deletepostpage" element={<DeletePostPage />} />
+          <Route path="/editcommentpage" element={<EditCommentPage />} />
         </Routes>
       </BrowserRouter>
     </>
